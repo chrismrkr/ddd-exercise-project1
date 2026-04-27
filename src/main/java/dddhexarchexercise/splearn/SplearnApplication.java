f0506d3c9f4a14445211f3e7bf7b8bf655e346c4
chrismrkr <b635032_@daum.net>
2026-04-28 00:31:40 +0900

    init

---
+++ src/main/java/dddhexarchexercise/splearn/SplearnApplication.java
@@ -0,0 +1,12 @@
+package dddhexarchexercise.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplearnApplication.class, args);
+	}
+}
